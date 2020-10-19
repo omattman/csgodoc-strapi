ff8da256f7419be430f4adf61990470bbb303773
Matthias Hansen <mnha18@student.aau.dk>
2020-10-19 17:09:38 +0200

    Add Mirage map

--- client/data/mapData.js
+++ client/data/mapData.js
@@ -3,7 +3,12 @@ const DATA = [
     mapTitle: "Inferno",
     to: "maps/inferno",
     overlaysrc: "/map-inferno.png"
-  }
+  },
+  {
+    mapTitle: "Mirage",
+    to: "maps/mirage",
+    overlaysrc: "/map-mirage.png"
+  },
 ];
 
 export default DATA;
